c980270e9f255a3a321e532e2fde7077fb75c342
Wadzhamah <semenbindu@gmail.com>
2023-09-29 20:15:43 +0300

    design replaced

--- Assets/MenuPanel.cs
+++ Assets/MenuPanel.cs
@@ -1,3 +1,5 @@
+using System;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -5,20 +7,41 @@ using UnityEngine.UI;
 public class MenuPanel : MonoBehaviour
 {
     [SerializeField]
+    private TMP_InputField _userNameInput;
+
+    [SerializeField]
     private Button _playButton;
     [SerializeField]
     private Button _infoButton;
     [SerializeField]
     private Button _exitButton;
 
+    [SerializeField]
+    private int maxUsernameLength = 10;
+
     private void Awake()
     {
         _playButton.onClick.AddListener(OnPlayButtonClick);
         _infoButton.onClick.AddListener(OnInfoButtonClick);
         _exitButton.onClick.AddListener(OnExitButtonClick);
+
+        _userNameInput.onEndEdit.AddListener(OnEndUsernameEdit);
+        _userNameInput.characterLimit = maxUsernameLength;
     }
 
-    
+    private void OnEndUsernameEdit(string arg0)
+    {
+        //if (arg0.Length > maxUsernameLength)
+        //{
+        //    _userNameInput.text = arg0.Substring(0, maxUsernameLength);
+        //    arg0 = _userNameInput.text;
+        //}
+
+        GlobalVariables.USER_NAME = arg0;
+
+        Debug.Log(GlobalVariables.USER_NAME);
+    }
+
     private void OnPlayButtonClick()
     {
         SceneManager.LoadScene("Game");
